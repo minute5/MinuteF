04dface9b516505cb23957c86b3f85c29da15f3d
李宗蔚 <zongw.lee@gmail.com>
2019-04-19 17:30:52 +0800

    [IMP] 跨域更改

---
+++ api-gateway/src/main/java/per/zongwlee/gateway/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package per.zongwlee.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author zongw.lee@gmail.com
+ * @since 2019/04/19
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(36000)
+                .allowCredentials(true);
+    }
+}
