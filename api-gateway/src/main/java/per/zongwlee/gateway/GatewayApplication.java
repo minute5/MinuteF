28dd608b044af7e8d76bd1dd3529cf182575ab71
李宗蔚 <zongw.lee@gmail.com>
2019-04-19 17:09:37 +0800

    [IMP] 跨域设置

--- api-gateway/src/main/java/per/zongwlee/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/per/zongwlee/gateway/GatewayApplication.java
@@ -9,6 +9,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import per.zongwlee.gateway.filter.AccessFilter;
 
 @EnableFeignClients("per.zongwlee.gateway")
@@ -26,5 +29,15 @@ public class GatewayApplication {
     @Bean
     public AccessFilter accessFilter() {
         return new AccessFilter();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*").allowedOrigins("*");
+            }
+        };
     }
 }
